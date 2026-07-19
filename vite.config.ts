9821e164bbaa12ac50cfc27e56b5b10941888a23
Voicu Andrei <dreivoicu@gmail.com>
2026-07-19 18:05:55 +0300

    reinitialize

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,12 @@
-import { defineConfig } from 'vite-plus';
-import type { UserConfig } from 'vite-plus';
-import vue from '@vitejs/plugin-vue';
-import path from 'node:path';
-
-const currentDir = path.basename(process.cwd());
-
-const getProjectConfig = (): UserConfig => {
-  switch (currentDir) {
-    case 'client':
-      return {
-        plugins: [vue()] as never,
-        server: {
-          proxy: {
-            '/api': {
-              target: 'http://localhost:3000',
-              changeOrigin: true,
-              secure: false,
-            },
-          },
-        },
-      };
-
-    case 'server':
-      return {
-        server: {
-          port: 3000,
-        },
-      };
-
-    default:
-      return {};
-  }
-};
+import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  staged: { '*': 'vp check --fix' },
-  fmt: { singleQuote: true },
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
   lint: { options: { typeAware: true, typeCheck: true } },
-  run: { cache: true },
-
-  ...getProjectConfig(),
+  run: {
+    cache: true,
+  },
 });
