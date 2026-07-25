ba501576d4a76392ba4d57aab7a3beef2ce6f036
Voicu Andrei <dreivoicu@gmail.com>
2026-07-25 08:51:01 +0300

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
-  staged: { '*': 'vp check --fix' },
+  staged: {
+    '*': 'vp check --fix',
+    '*.{ts,vue}': 'vp test related --run',
+  },
   fmt: { singleQuote: true },
   lint: { options: { typeAware: true, typeCheck: true } },
   run: { cache: true },
