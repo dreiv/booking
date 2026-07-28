18a0c2f304e1d4bc2627de3e73b57fb5644beefd
Voicu Andrei <dreivoicu@gmail.com>
2026-07-28 12:14:31 +0300

    remove unused openapi

--- apps/server/scripts/generate-openapi.ts
+++
@@ -1,7 +0,0 @@
-import { writeFileSync } from 'node:fs';
-import { generateOpenApiDocument } from '../src/openapi/registry.ts';
-
-writeFileSync(
-  new URL('../openapi.json', import.meta.url),
-  JSON.stringify(generateOpenApiDocument(), null, 2),
-);
